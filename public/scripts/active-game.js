4746bbae39e364a157223aa2b29fd0735a8fe47a
George Antonakos <gantonakos2003@yahoo.gr>
2020-03-05 00:28:09 +0100

    update values and login screen

--- public/scripts/active-game.js
+++ public/scripts/active-game.js
@@ -6,18 +6,59 @@ import { ItcObjectivesSecondariesComponent } from "./components/itc-objectives-s
 var db = firebase.firestore();
 
 const ActiveGameComponent = {
-    data: () => ({ activeGameData: [] }),
+    data: () => ({ activeGameData: [], activeGamePlayers: [], userLoggedIn: false, user: -1, showMessage: false }),
     created() {
-        this.getDataFromDB()
+        this.getDataFromDB();
     },
     methods: {
         getDataFromDB() {
             db.collection("players").onSnapshot(query => {
                 this.activeGameData = [];
                 query.forEach(players => {
+                    this.activeGamePlayers.push(players.id);
                     this.activeGameData.push(players.data());
                 });
+                this.checkLoggedIn();
             });
+        },
+        onUpdate(data) {
+            this.updateData(data.key, data.id, data.add, data.parent ? data.parent : false);
+        },
+        updateData(key, id, add, parent) {
+            var player = this.activeGameData.filter(data => data.id === id)[0];
+            var playerIndex = this.activeGameData.indexOf(player);
+            if (!parent) {
+                player[key] = add ? player[key] + 1 : player[key] - 1;
+            } else {
+                (player[parent])[key] = add ? (player[parent])[key] + 1 : (player[parent])[key] - 1;
+            }
+            db.collection("players").doc(this.activeGamePlayers[playerIndex]).update(player);
+        },
+        login() {
+            db.collection("players").where("name", "==", this.$refs.login.value).get().then(query => {
+                this.showMessage = false;
+                if (query.empty) {
+                    this.showMessage = true;
+                }
+
+                query.forEach(player => {
+                    this.user = player.data().id;
+                    this.userLoggedIn = true;
+                    localStorage.setItem("user", player.data().id);
+                });
+            });
+        },
+        checkLoggedIn() {
+            var localUser = localStorage.getItem("user");
+            if (localUser) {
+                this.userLoggedIn = true;
+                this.user = localUser;
+            }
+        },
+        logout() {
+            this.userLoggedIn = false;
+            this.user = "";
+            localStorage.removeItem("user");
         }
     },
     components: {
@@ -27,22 +68,40 @@ const ActiveGameComponent = {
         "itc-objectives-secondaries-component": ItcObjectivesSecondariesComponent
     },
     template: `<div class="main-container">
-    <game-points-component v-bind:players="activeGameData"></game-points-component>
-    <h4 class="ui horizontal divider header">
-        <i class="icon icon-bolter-mkv"></i>
-        Details
-    </h4>
-    <div class="ui two column grid">
-        <command-points-component v-bind:players="activeGameData"></command-points-component>
-        <itc-objectives-component v-bind:players="activeGameData"></itc-objectives-component>
-        <itc-objectives-secondaries-component v-bind:players="activeGameData"></itc-objectives-secondaries-component>
+    <div class="active-game" v-if='userLoggedIn'>
+        <game-points-component :user="user" :players="activeGameData" @gamePoints=onUpdate></game-points-component>
+        <h4 class="ui horizontal divider header">
+            <i class="icon icon-bolter-mkv"></i>
+            Details
+        </h4>
+        <div class="ui two column grid">
+            <command-points-component :user="user" :players="activeGameData" @gamePoints=onUpdate></command-points-component>
+            <itc-objectives-component :user="user" :players="activeGameData" @gamePoints=onUpdate></itc-objectives-component>
+            <itc-objectives-secondaries-component :user="user" :players="activeGameData" @gamePoints=onUpdate></itc-objectives-secondaries-component>
+        </div>
+        <div class="ui divider"></div>
+        <div class="two ui buttons">
+            <div class="ui button basic mini" @click=logout>Logout</div>
+            <div class="ui button basic red">Finish Game</div>
+        </div>
+        <div class="ui divider"></div>
     </div>
-    <div class="ui divider"></div>
-    <div class="two ui buttons">
-        <div class="ui button basic">Cancel</div>
-        <div class="ui button basic red">Finish Game</div>
+    <div class="login" v-if='!userLoggedIn'>
+        <div class="ui segment raised">
+            <h1>Login</h1>
+            <div class="ui action fluid input">
+                <input type="text" placeholder="Enter User's Name" ref="login">
+                <button class="ui button" @click=login>Login</button>
+            </div>
+        </div>
+        <div class="ui negative message" v-if='showMessage'>
+            <div class="header">
+                The user does not exist
+            </div>
+            <p>Please try again!</p>
+        </div>
     </div>
-    <div class="ui divider"></div>
+    
     </div>`
 };
 
--- public/scripts/components/itc-objectives-secondaries.js
+++ public/scripts/components/itc-objectives-secondaries.js
@@ -11,19 +11,19 @@ const objectives = ["hh", "ks", "os", "e", "mfd", "bel"];
 
 const ItcObjectivesSecondariesComponent = {
     data: () => ({ objectives: objectives, objectiveText: objectiveText }),
-    props: ["players"],
+    props: ["players", "user"],
     template: `
     <div class="row">
         <div class="column" v-for="player in players">
             <h5>Secondary Objectives:</h5>
             <div class="ui middle aligned divided list">
-                <div class="item" v-for="objective in objectives" v-if="player.secondaries[objective] != undefined">
+                <div class="item objectives" v-for="objective in objectives" v-if="player.secondaries[objective] != undefined">
                     <div class="right floated content">
-                        <button class="ui button icon mini">
+                        <button class="ui button icon mini" @click="$emit('gamePoints', { id: player.id, add: false, key: objective, parent: 'secondaries' })" v-if="player.id == user">
                             <i class="minus icon"></i>
                         </button>
                         {{ player.secondaries[objective] }}
-                        <button class="ui button icon mini">
+                        <button class="ui button icon mini" @click="$emit('gamePoints', { id: player.id, add: true, key: objective, parent: 'secondaries' })" v-if="player.id == user">
                             <i class="plus icon"></i>
                         </button>
                     </div>
